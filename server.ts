0f6c8d7139c049e2468a70a3bd7df931edae730d
Lê Vạn Bảo Trọng <32262183+chuanghiduoc@users.noreply.github.com>
2024-11-05 00:05:33 +0700

    feat: initialize GraphQL with Express and MongoDB

--- server.ts
+++ server.ts
@@ -1,16 +1,34 @@
 import dotenv from 'dotenv';
 import { Server } from 'http';
 import app from './src/app';
+import cluster from 'cluster';
+import os from 'os';
 
 const envFile = process.env.NODE_ENV === 'production' ? '.env' : '.env.local';
 dotenv.config({ path: envFile });
 
 const PORT = process.env.PORT || 4000;
+const numCPUs = os.cpus().length;
 
-const server: Server = app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+if (cluster.isPrimary) {
+  // Fork workers for each CPU core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-process.on('SIGINT', () => {
-  server.close(() => console.log(`Express server exited`));
-});
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, signal: ${signal}`);
+  });
+} else {
+  // Workers can share any TCP connection
+  const server: Server = app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT} by worker ${process.pid}`);
+  });
+
+  process.on('SIGINT', () => {
+    server.close(() => {
+      console.log(`Express server exited for worker ${process.pid}`);
+      process.exit(0); // Ensure the worker exits
+    });
+  });
+}
